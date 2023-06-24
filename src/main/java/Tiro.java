7ead9374c8e44ed8eb5a8dcdec4a61b2a104af4e
Exetria <107202768+Exetria@users.noreply.github.com>
2023-06-25 02:37:30 +0700

    SkyBoxCube selesai - Tiro

--- src/main/java/Tiro.java
+++ src/main/java/Tiro.java
@@ -227,27 +227,3 @@ public class Tiro
         }
     }
 }
-
-class TextureData
-{
-    private int width, height;
-    private ByteBuffer data;
-
-    public TextureData(ByteBuffer data, int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.data = data;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public ByteBuffer getBuffer() {
-        return data;
-    }
-}
