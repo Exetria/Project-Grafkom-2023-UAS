2c94bcb9cc95141b5bd690117ab53a68ffea5d7a
HannClement <clementgunadi@gmail.com>
2023-06-25 15:12:49 +0700

    Object Clement semifinal

--- src/main/java/Clement.java
+++ src/main/java/Clement.java
@@ -1,12 +1,8 @@
 import Engine.*;
-import Engine.Object;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
 import org.lwjgl.opengl.GL;
 
-import java.io.File;
-import java.nio.ByteBuffer;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,14 +12,14 @@ import static org.lwjgl.opengl.GL20.*;
 
 public class Clement
 {
-    private final Window window = new Window(1000, 1000, "window");
+    private final Window window = new Window(1920, 1080, "window");
     Camera camera = new Camera();
     Projection projection = new Projection(window.getWidth(), window.getHeight());
 
     SkyBoxCube skybox;
-    ArrayList<Object> objects = new ArrayList<>();
-    ArrayList<Sphere> spheres = new ArrayList<>();
+    ArrayList<Objects> spheres = new ArrayList<>();
 
+    boolean cameraMode = true;
     float movement= 0.1f;
 
     public static void main(String[] args)
@@ -42,43 +38,33 @@ public class Clement
         window.init();
         GL.createCapabilities();
         glEnable(GL_DEPTH_TEST);
-        camera.setPosition(0, 0,  0.5f);
-        camera.setRotation((float) Math.toRadians(0f),  (float) Math.toRadians(0f));
+//        camera.setPosition(0, 40,  -60f);
+        camera.setPosition(0, 0f,0f);
+//        camera.setRotation((float) Math.toRadians(25f),  (float) Math.toRadians(180f));
 
         skybox = new SkyBoxCube();
 
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
                         new ArrayList<>(),
-                        new Vector4f(1f, 1f, 0.87f, 1.0f), new ArrayList<>(),
-                        "C:\\Users\\Clement\\Intellij\\Project-Grafkom-2023-UAS\\resources\\objects\\Clement\\matahari.obj"
-                )
-        );
-        spheres.get(0).translateObject(-5f, 10f, 20f);
-
-        spheres.add(new Sphere
-                (
-                        Arrays.asList
-                                (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
-                        new ArrayList<>(),
-                        new Vector4f(0.98f, 0.84f, 0.39f, 1.0f), new ArrayList<>(),
+                        new Vector4f(0.98f, 0.83f, 0.64f, 1.0f), new ArrayList<>(),
                         "C:\\Users\\Clement\\Intellij\\Project-Grafkom-2023-UAS\\resources\\objects\\Clement\\badanMobil.obj"
                 )
         );
 
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
                         new ArrayList<>(),
-                        new Vector4f(0.5f, 0.5f, 0.5f, 1.0f), new ArrayList<>(),
+                        new Vector4f(0.5f, 0.52f, 0.53f, 1.0f), new ArrayList<>(),
                         "C:\\Users\\Clement\\Intellij\\Project-Grafkom-2023-UAS\\resources\\objects\\Clement\\banDalamMobil.obj"
                 )
         );
 
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
@@ -88,7 +74,7 @@ public class Clement
                 )
         );
 
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
@@ -98,17 +84,7 @@ public class Clement
                 )
         );
 
-        spheres.add(new Sphere
-                (
-                        Arrays.asList
-                                (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
-                        new ArrayList<>(),
-                        new Vector4f(0.0f, 1.0f, 0.0f, 1.0f), new ArrayList<>(),
-                        "C:\\Users\\Clement\\Intellij\\Project-Grafkom-2023-UAS\\resources\\objects\\Clement\\senjataMobil.obj"
-                )
-        );
-
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
@@ -118,7 +94,7 @@ public class Clement
                 )
         );
 
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
@@ -128,57 +104,98 @@ public class Clement
                 )
         );
 
-        spheres.add(new Sphere
+        spheres.add(new Objects
                 (
                         Arrays.asList
                                 (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
                         new ArrayList<>(),
-                        new Vector4f(1.0f, 0.45f, 0.09f, 0.0f), new ArrayList<>(),
+                        new Vector4f(1.0f, 0.45f, 0.09f, 1.0f), new ArrayList<>(),
                         "C:\\Users\\Clement\\Intellij\\Project-Grafkom-2023-UAS\\resources\\objects\\Clement\\lampuOrange.obj"
                 )
         );
 
-
-
-
+        spheres.add(new Objects
+                (
+                        Arrays.asList
+                                (new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER), new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER)),
+                        new ArrayList<>(),
+                        new Vector4f(0.0f, 1.0f, 0.0f, 1.0f), new ArrayList<>(),
+                        "C:\\Users\\Clement\\Intellij\\Project-Grafkom-2023-UAS\\resources\\objects\\Clement\\senjataMobil.obj"
+                )
+        );
 
 
     }
-
-
 
     public void input()
     {
         //WASDQE BUAT ROTATE ATAU TRANSLATE CAMERA
         {
-            if(window.isKeyPressed(GLFW_KEY_Q))
+            if(cameraMode)
             {
-                camera.moveDown(movement);
-            }
+                if(window.isKeyPressed(GLFW_KEY_Q))
+                {
+                    camera.moveDown(movement);
+                }
 
-            if(window.isKeyPressed(GLFW_KEY_E))
-            {
-                camera.moveUp(movement);
-            }
+                if(window.isKeyPressed(GLFW_KEY_E))
+                {
+                    camera.moveUp(movement);
+                }
 
-            if(window.isKeyPressed(GLFW_KEY_W))
-            {
-                camera.moveForward(movement);
-            }
+                if(window.isKeyPressed(GLFW_KEY_W))
+                {
+                    camera.moveForward(movement);
+                }
 
-            if(window.isKeyPressed(GLFW_KEY_S))
-            {
-                camera.moveBackwards(movement);
-            }
+                if(window.isKeyPressed(GLFW_KEY_S))
+                {
+                    camera.moveBackwards(movement);
+                }
 
-            if(window.isKeyPressed(GLFW_KEY_A))
-            {
-                camera.moveLeft(movement);
-            }
+                if(window.isKeyPressed(GLFW_KEY_A))
+                {
+                    camera.moveLeft(movement);
+                }
 
-            if(window.isKeyPressed(GLFW_KEY_D))
+                if(window.isKeyPressed(GLFW_KEY_D))
+                {
+                    camera.moveRight(movement);
+                }
+            }
+            else
             {
-                camera.moveRight(movement);
+                if(window.isKeyPressed(GLFW_KEY_Q))
+                {
+                    camera.moveUp(movement);
+                }
+
+                if(window.isKeyPressed(GLFW_KEY_E))
+                {
+                    camera.moveUp(movement);
+                }
+
+                if(window.isKeyPressed(GLFW_KEY_W))
+                {
+                    camera.moveForward(movement);
+                }
+
+                if(window.isKeyPressed(GLFW_KEY_S))
+                {
+                    camera.moveBackwards(movement);
+                }
+
+                if(window.isKeyPressed(GLFW_KEY_A))
+                {
+                    camera.moveLeft(movement);
+                    camera.rotateTowardsPoint(0, -0.01f, spheres.get(0).getCenterPoint().get(0), spheres.get(0).getCenterPoint().get(1), spheres.get(0).getCenterPoint().get(2));
+                }
+
+                if(window.isKeyPressed(GLFW_KEY_D))
+                {
+                    camera.moveRight(movement);
+                    camera.rotateTowardsPoint(0, 0.01f, spheres.get(0).getCenterPoint().get(0), spheres.get(0).getCenterPoint().get(1), spheres.get(0).getCenterPoint().get(2));
+                }
             }
         }
 
@@ -213,7 +230,7 @@ public class Clement
         {
             if(window.isKeyPressed(GLFW_KEY_U))
             {
-                for (Sphere i: spheres)
+                for (Objects i: spheres)
                 {
                     i.translateObject(0f, 0f, 0.001f);
                 }
@@ -221,7 +238,7 @@ public class Clement
 
             if(window.isKeyPressed(GLFW_KEY_O))
             {
-                for (Sphere i: spheres)
+                for (Objects i: spheres)
                 {
                     i.translateObject(0f, 0f, -0.001f);
                 }
@@ -229,7 +246,7 @@ public class Clement
 
             if(window.isKeyPressed(GLFW_KEY_I))
             {
-                for (Sphere i: spheres)
+                for (Objects i: spheres)
                 {
                     i.translateObject(0f, 0.001f, 0f);
                 }
@@ -237,7 +254,7 @@ public class Clement
 
             if(window.isKeyPressed(GLFW_KEY_K))
             {
-                for (Sphere i: spheres)
+                for (Objects i: spheres)
                 {
                     i.translateObject(0f, -0.001f, 0f);
                 }
@@ -245,7 +262,7 @@ public class Clement
 
             if(window.isKeyPressed(GLFW_KEY_J))
             {
-                for (Sphere i: spheres)
+                for (Objects i: spheres)
                 {
                     i.translateObject(-0.001f, 0f, 0f);
                 }
@@ -253,18 +270,35 @@ public class Clement
 
             if(window.isKeyPressed(GLFW_KEY_L))
             {
-                for (Sphere i: spheres)
+                for (Objects i: spheres)
                 {
                     i.translateObject(0.001f, 0f, 0f);
                 }
+            }
+
+            if (window.isKeyPressed(GLFW_KEY_E))
+            {
+
             }
         }
 
         //================================================================================
 
+        if(window.isKeyPressed(GLFW_KEY_F))
+        {
+            cameraMode = false;
+            System.out.println("Alternative Camera Mode!");
+        }
+
+        if(window.isKeyPressed(GLFW_KEY_G))
+        {
+            cameraMode = true;
+            System.out.println("Normal Mode");
+        }
+
         if(window.isKeyPressed(GLFW_KEY_LEFT_SHIFT))
         {
-            for (Sphere i: spheres)
+            for (Objects i: spheres)
             {
                 camera.moveUp(movement);
             }
@@ -272,7 +306,7 @@ public class Clement
 
         if(window.isKeyPressed(GLFW_KEY_LEFT_CONTROL))
         {
-            for (Sphere i: spheres)
+            for (Objects i: spheres)
             {
                 camera.moveDown(movement);
             }
@@ -283,6 +317,7 @@ public class Clement
             Vector2f displayVector = window.getMouseInput().getDisplVec();
             camera.addRotation((float) Math.toRadians(displayVector.x * 0.1f), (float) Math.toRadians(displayVector.y * 0.1f));
         }
+
         if(window.getMouseInput().getScroll().y != 0)
         {
             projection.setFOV(projection.getFOV() - (window.getMouseInput().getScroll().y * 0.01f));
@@ -300,7 +335,7 @@ public class Clement
             GL.createCapabilities();
 
             //Code
-            for (Sphere objects : this.spheres)
+            for (Objects objects : this.spheres)
             {
                 //gambar sekalian child
                 objects.draw(camera, projection);
